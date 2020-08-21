b2de9e4a37456398e7b7711a3a38fab1f890bad7
Joni <lezargabriel10@gmail.com>
2020-08-21 14:28:43 +0700

    belajar state

--- App.js
+++ App.js
@@ -8,65 +8,145 @@ import {
   Text,
   StatusBar,
   Button,
-  TouchableOpacity
+  TouchableOpacity,
+  TextInput
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 
-class App extends Component{
-  render(){
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      nama: '-',
+      username: '',
+      password: '',
+      isLogin: false
+    }
+
+
+  }
+  render() {
     return (
-      <View>
-        <Text>ANJAY !</Text>
-        
-          <View style={{flexDirection:"row",width:"100%", backgroundColor:"orange", height:40}}>
-            <Text style={{backgroundColor:"#0000FF",color:"white",flex:1, height:20}}>Lezar</Text>
-            <Text style={{backgroundColor:"#03942a",color:"white",flex:1,textAlign:"center"}}>Mamzky</Text>
-            <Text style={{backgroundColor:"#94036b",color:"white",flex:1}}>Joni</Text>
-          </View>
+      <View style={{ backgroundColor: "#f5f5f5", padding: 5 }}>
+        <Text style={{ fontSize: 50, width: "100%", textAlign: "center" }}>{this.state.nama}</Text>
+        <View style={{ width: "100%", height: 300, backgroundColor: "red", padding: 10 }}>
 
-          <View style={{flexDirection:"row",width:"100%", backgroundColor:"orange", height:30}}>
+          {/* INPUT UNSERNAME */}
+          <Text style={{ fontSize: 20, color: "white", width: "100%", textAlign: "center" }}>Username</Text>
+          <TextInput
+            style={{ width: '100%', backgroundColor: 'white', borderRadius: 10 }}
+            value={this.state.username}
+            onChangeText={(value) => this.setState({ username: value })}
+          />
+
+          {/* INPUT PASSWORD */}
+          <Text style={{ fontSize: 20, color: "white", width: "100%", textAlign: "center" }}>Password</Text>
+          <TextInput
+            style={{ width: '100%', backgroundColor: 'white', borderRadius: 10 }}
+            value={this.state.password}
+            onChangeText={(value) => this.setState({ password: value })}
+          />
+          
+          {/* BUTTON LOGIN */}
+          {!this.state.isLogin && (<TouchableOpacity
+            style={{ width: 100, height: 40, backgroundColor: 'green', borderRadius: 15, marginTop: 20 }}
+            onPress={() => this.validasi(this.state.username, this.state.password)}
+          >
+            <Text style={{ width: '100%', textAlign: "center" }}>login</Text>
+          </TouchableOpacity>)}
+
+          {/* BUTTON LOGOUT */}
+          {this.state.isLogin && (<TouchableOpacity
+            style={{ width: 100, height: 40, backgroundColor: 'gray', borderRadius: 15, marginTop: 20, color: 'white' }}
+            onPress={() => this.logOut()}
+          >
+            <Text style={{ width: '100%', textAlign: "center" }}>logout</Text>
+          </TouchableOpacity>)}
+        </View>
+
+        <View style={{ flexDirection: "row", width: "100%", backgroundColor: "orange", height: 40 }}>
+          <Text style={{ backgroundColor: "#0000FF", color: "white", flex: 1, height: 20 }}>Lezar</Text>
+          <Text style={{ backgroundColor: "#03942a", color: "white", flex: 1, textAlign: "center" }}>Mamzky</Text>
+          <Text style={{ backgroundColor: "#94036b", color: "white", flex: 1 }}>Joni</Text>
+        </View>
+
+        <View style={{ flexDirection: "row", width: "100%", backgroundColor: "orange", height: 30 }}>
 
           <TouchableOpacity
-          style={ddd.stylenyaButton}
+            style={ddd.stylenyaButton}
             onPress={() => this.munculinPopup()}
           >
             <Text>Press me 1</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
-          style={ddd.stylenyaButton}
-            onPress={() => this.nampilinPopup()}
+            style={ddd.stylenyaButton}
+            onPress={() => this.validasi(this.state.username, this.state.password)}
           >
-            <Text style={{color:"white"}}>Press me 1</Text>
+            <Text style={{ color: "white" }}>CHECK !</Text>
           </TouchableOpacity>
-          </View>
-          
+        </View>
+        <View>
+          <Text style={{ fontSize: 40 }}>{this.state.nama}</Text>
+        </View>
       </View>
     );
   }
 
-  munculinPopup(){
-    alert('Hello World')
+  munculinPopup() {
+    alert('usernamenya admin')
   }
-  nampilinPopup(){
-    alert('Hello Grave')
+  nampilinPopup() {
+    alert('usernamenya bukan admin')
+  }
+
+  validasi(uname, pw) {
+    let accUsername = 'admin'
+    let accPassword = 'admin'
+
+    if (uname == accUsername && pw == accPassword) {
+      this.suksesLogin()
+    } else if (uname != accUsername && pw == accPassword) {
+      this.usernameSalah()
+    } else if (uname == accUsername && pw != accPassword) {
+      this.passwordSalah()
+    } else if (uname == '' && pw == '') {
+      alert(' username dan password harus diisi')
+    } else {
+      this.gagalLogin()
+    }
+  }
+
+  suksesLogin() {
+    alert('sukses login')
+    this.setState({ isLogin: true, nama: this.state.username })
+    this.clearLogin()
+  }
+  gagalLogin() {
+    alert('gagal login')
+  }
+  usernameSalah() {
+    alert("username salah")
+  }
+  passwordSalah() {
+    alert('password salah')
+  }
+  clearLogin() {
+    this.setState({ username: "", password: '' })
+  }
+  logOut() {
+    this.setState({ isLogin: false, nama: '-' })
   }
 }
 
 
 const ddd = StyleSheet.create({
-  stylenyaButton:{
-    margin:5,
-    flex:1,
-    backgroundColor:"blue"
+  stylenyaButton: {
+    margin: 5,
+    flex: 1,
+    backgroundColor: "blue"
   }
 });
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 CMjoni
+
+
+command untuk running emulator
+buat membuka list emulator yang tersedia
+emulator -list-avds
+running avd untuk contoh hp google pixel = emulator -avd pixel
+untuk merunning project android/ios melalui react native
+react-native run-android - react-native run-ios
